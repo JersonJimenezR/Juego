6cfd8d55f7338d53d8efd203b85dd6b31316bf0d
Jonathan Mafla <jonathan_-jhm@utp.edu.co>
2019-06-18 19:30:16 -0500

    Actualizando.

--- game.py
+++ game.py
@@ -1,36 +1,186 @@
+# imagenRecortada = objects.recortarImagen( 8, 12, 'animales', [12,12,12,12,12,12,12,12] )
+# contador = 0
 
-import pygame, math, fn, random, clases, objects, sprites
+# contador += 1
+#
+# if contador > 2:
+#     contador = 0
 
-
+import pygame, gm, math, random, obj
 
 if __name__ == '__main__':
 
-    ANCHO = 800
-    ALTO = 800
+    size = [620,465]
+    pantalla=gm.pantalla("Juego_Goku",size)
+    fin =False
+    gameover = False
+    life = 20
+    score = 0
+    level=200
 
-    fin = False
-    pantalla = fn.iniciar( [ANCHO,ALTO] )
+    # Grupos de sprites
+    muertes=pygame.sprite.Group()
+    estados=pygame.sprite.Group()
+    fondos=pygame.sprite.Group()
+    jugadores=pygame.sprite.Group()
+    poderes=pygame.sprite.Group()
+    rivalpoderes=pygame.sprite.Group()
+    rivales=pygame.sprite.Group()
 
-    imagenRecortada = objects.recortarImagen( 8, 12, 'animales', [12,12,12,12,12,12,12,12] )
-    contador = 0
+    # creación del fondo
+    f=obj.Fondo(spt.fondo)
+    fondos.add(f)
 
+    # creación del jugador
+    pto=[310,410] #ubicación inicial del jugador
+    vj=[3,3] #velocidad del jugador
+    j=obj.Jugador(pto,gm.cargarImagen('goku.png'))
+    jugadores.add(j)
 
+    # creación de n rivales
+    gm.CreateRv(spt.saibar,10,rivales)
 
-while not fin:
+while not (fin or gameover):
 
+    # Movimientos de personaje
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             fin = True
 
-    fn.limpiarPantalla( pantalla )
-    pantalla.blit( imagenRecortada[0][ contador ] , [40,40])
-    fn.refresh()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                jugadores.remove(j)
+                j = obj.Jugador([j.rect.x,j.rect.y],spt.gokuu)
+                jugadores.add(j)
+                j.vely = -vj[1]
+                j.velx = 0
+            if event.key == pygame.K_DOWN:
+                jugadores.remove(j)
+                j = obj.Jugador([j.rect.x,j.rect.y],spt.gokud)
+                jugadores.add(j)
+                j.vely = vj[1]
+                j.velx = 0
+            if event.key == pygame.K_LEFT:
+                jugadores.remove(j)
+                j = obj.Jugador([j.rect.x,j.rect.y],spt.gokul)
+                jugadores.add(j)
+                j.velx = -vj[0]
+                j.vely = 0
+            if event.key == pygame.K_RIGHT:
+                jugadores.remove(j)
+                j = obj.Jugador([j.rect.x,j.rect.y],spt.gokur)
+                jugadores.add(j)
+                j.velx = vj[0]
+                j.vely = 0
+            if event.key == pygame.K_SPACE:
+                # creación de la bala
+                jugadores.remove(j)
+                j = obj.Jugador([j.rect.x,j.rect.y],spt.gokust)
+                jugadores.add(j)
+                p = obj.Poder([j.rect.x,j.rect.y],spt.poder)
+                p.vely = -3
+                poderes.add(p)
 
-    contador += 1
+        if event.type == pygame.KEYUP:
+            jugadores.remove(j)
+            j = obj.Jugador([j.rect.x,j.rect.y],spt.goku)
+            jugadores.add(j)
+            j.vely = 0
+            j.velx = 0
 
-    if contador > 2:
-        contador = 0
+    # Control de jugador y rivales en pantalla
+    for j in jugadores:
 
+        if j.rect.x > (size[0] + j.rect.width) :
+            j.rect.x = 0
+            j.velx = vj[0]
 
-    fn.reloj().tick(5)
+        if j.rect.x < 0 - j.rect.height:
+            j.rect.x = size[0]
+            j.velx = -vj[0]
+
+        if j.rect.y > (size[1] + j.rect.height) :
+            j.rect.y = 0
+            j.vely = vj[1]
+
+        if j.rect.y < 0 - j.rect.height:
+            j.rect.y = size[1]
+            j.vely = -vj[1]
+    for r in rivales:
+        if r.rect.x > (size[0] - r.rect.width) :
+            rivales.remove(r)
+            r = obj.Rival([r.rect.x,r.rect.y],spt.saibal)
+            r.velx = -2
+            rivales.add(r)
+        if r.rect.x < 0:
+            rivales.remove(r)
+            r = obj.Rival([r.rect.x,r.rect.y],spt.saibar)
+            r.velx = 2
+            rivales.add(r)
+        if random.randrange(level)>=int(level*0.999):
+            rivales.remove(r)
+            rs = obj.Rival([r.rect.x,r.rect.y],spt.saibast)
+            rs.velx = r.velx
+            rivales.add(rs)
+            rp = obj.Poder([r.rect.x,r.rect.y],spt.rivalpoder)
+            rp.vely = 2
+            rivalpoderes.add(rp)
+
+    # Ataques
+    for p in poderes:
+        for r in rivales:
+            if pygame.sprite.collide_rect(r,p):
+                rivales.remove(r)
+                poderes.remove(p)
+                score += 1
+                m = obj.Muerte([r.rect.x,r.rect.y],spt.muerte)
+                muertes.add(m)
+        if p.rect.y < -50:
+            poderes.remove(p)
+        if len(rivales) <= 3: #len = cantidad en grupo
+            gm.CreateRv(spt.saibar,6,rivales)
+    for rp in rivalpoderes:
+        for j in jugadores:
+            if pygame.sprite.collide_rect(rp,j):
+                rivalpoderes.remove(rp)
+                life -= 1
+                m = obj.Muerte([j.rect.x,j.rect.y],spt.muerte)
+                muertes.add(m)
+        if rp.rect.y > size[1]+50:
+            poderes.remove(rp)
+    for m in muertes:
+        if m.time==0:
+            muertes.remove(m)
+
+    # Final del Juego
+    if life == 0:
+        gameover = True
+
+    # Indicadores, estados, dibujado y refresco
+    # Indicadores
+    fondos.draw(pantalla)
+    gm.StGoku(life,estados,[5,5])
+    gm.health(pantalla,life)
+    gm.score(pantalla,score)
+    # Estado
+    estados.draw(pantalla)
+    jugadores.update()
+    rivales.update()
+    poderes.update()
+    rivalpoderes.update()
+    muertes.update()
+    # Dibujado
+    jugadores.draw(pantalla)
+    rivales.draw(pantalla)
+    rivalpoderes.draw(pantalla)
+    poderes.draw(pantalla)
+    muertes.draw(pantalla)
+    # Refresco
+    gm.up()
+    gm.reloj().tick(400)
+
+gm.gameover(pantalla,score)
+while not (fin):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            fin = True
---
+++ obj.py
@@ -0,0 +1,64 @@
+import pygame
+
+
+class Estado (pygame.sprite.Sprite):
+    def __init__(self,pto,imagen):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = imagen
+        self.rect = self.image.get_rect()
+        self.rect.x = pto[0]
+        self.rect.y = pto[1]
+
+class Rival (pygame.sprite.Sprite):
+    def __init__(self,pto,imagen):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = imagen
+        self.rect = self.image.get_rect()
+        self.rect.x = pto[0]
+        self.rect.y = pto[1]
+        self.velx=2
+        self.vely=2
+    def update( self ):
+        self.rect.x += self.velx
+        # self.rect.y += self.vely
+
+class Fondo (pygame.sprite.Sprite):
+    def __init__(self,imagen):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = imagen
+        self.rect = self.image.get_rect()
+
+class Jugador (pygame.sprite.Sprite):
+    def __init__(self,pto,imagen):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = imagen
+        self.rect = self.image.get_rect()
+        self.rect.x = pto[0]
+        self.rect.y = pto[1]
+        self.velx=0
+        self.vely=0
+    def update( self ):
+        self.rect.x += self.velx
+        self.rect.y += self.vely
+
+class Poder (pygame.sprite.Sprite):
+    def __init__(self,pto,imagen):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = imagen
+        self.rect = self.image.get_rect()
+        self.rect.x = pto[0]
+        self.rect.y = pto[1]
+        self.vely=0
+    def update( self ):
+        self.rect.y += self.vely
+
+class Muerte (pygame.sprite.Sprite):
+    def __init__(self,pto,imagen):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = imagen
+        self.rect = self.image.get_rect()
+        self.rect.x = pto[0]
+        self.rect.y = pto[1]
+        self.time=80
+    def update(self):
+        self.time-=1
